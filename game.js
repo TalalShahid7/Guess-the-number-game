94bc0de8c9482baacad68cb3e675b593bd25910a
TalalShahid <talalshahid777@gmail.com>
2024-04-27 14:36:31 +0500

    Initial Commit

--- game.js
+++ game.js
@@ -1,10 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-
-let exe; 
-
+let exe;
 const randomnum = Math.floor(Math.random() * 100 + 1);
-
 do {
     const guessnum = await inquirer.prompt([
         {
@@ -14,13 +11,12 @@ do {
         },
     ]);
     console.log("Your num is", guessnum.gnum);
-
     if (parseInt(guessnum.gnum) === randomnum) {
         console.log("You guessed the num");
-    } else {
+    }
+    else {
         console.log("Not guessed");
     }
-
     let sectry = await inquirer.prompt([
         {
             name: "stry",
@@ -30,10 +26,10 @@ do {
         },
     ]);
     // (sectry.stry==='yes')?exe=true:exe=false;
-     if(sectry.stry==='yes'){
-        exe=true;
-     }
-     else{
-        exe=false;
-     }
+    if (sectry.stry === 'yes') {
+        exe = true;
+    }
+    else {
+        exe = false;
+    }
 } while (exe);
